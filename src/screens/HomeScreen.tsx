1ac6b16114196396feff1cca7ecb99c444a3699d
Motcrash <Motcrash@gmail.com>
2023-07-25 17:40:59 -0600

    Show Pokemons

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,57 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, Image, FlatList, ActivityIndicator } from 'react-native'
+import { styles } from '../theme/appTheme'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { usePokemonPagination } from '../hooks/usePokemonPagination'
+import { FadeInImage } from '../components/FadeInImage'
 
 export const HomeScreen = () => {
+
+    const { top } = useSafeAreaInsets();
+    const { simplePokemonList, loadPokemons } = usePokemonPagination();
+
    return (
-       <View>
-            <Text>HomeScreen</Text>
-       </View>
+       <>   
+
+            <Image
+                source={ require('../assets/pokebola.png')}
+                style={ styles.pokebolaBG }
+            />
+            
+            {/* <Text style={{
+                ...styles.title,
+                ...styles.globalMargin,
+                top: top+20
+            }}>Pokedex</Text> */}
+
+            <FlatList
+                data={ simplePokemonList }
+                keyExtractor={ ( poke ) => poke.id }
+                showsVerticalScrollIndicator={ false }
+                renderItem={({ item }) => (
+                    <FadeInImage
+                        uri={ item.picture }  
+                        style={{
+                            width: 100,
+                            height: 100
+                        }}
+                    />
+                ) }
+
+                // InfiniteScroll
+                onEndReached={ loadPokemons }
+                onEndReachedThreshold={ 0.4 }
+
+                // Activity indicator
+                ListFooterComponent={(
+                    <ActivityIndicator 
+                        style={{ height: 100}}
+                        color='red'
+                        size={ 30 }
+                    />
+                )}
+            /> 
+
+       </>
    )
 }
